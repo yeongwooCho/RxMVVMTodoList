d0683c9e88aaf0371b00d068dcbc8976720be0b7
yeongwoo <jyy0223@naver.com>
2021-09-23 01:03:35 +0900

    add UICompoenets

---
+++ RxMVVMTodoList/UIComponents.swift
@@ -0,0 +1,14 @@
+//
+//  UIComponents.swift
+//  RxMVVMTodoList
+//
+//  Created by 조영우 on 2021/09/23.
+//
+
+import UIKit
+
+struct UIComponents {
+    static var mainStoryboard: UIStoryboard {
+        return UIStoryboard(name: "main", bundle: Bundle.main)
+    }
+}
