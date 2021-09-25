24e8d229b98bc8d4e46d04ea8b76e6b98105975d
yeongwoo <jyy0223@naver.com>
2021-09-25 22:16:31 +0900

    fix refactoring in view model network call

--- RxMVVMTodoList/ViewModel/TodoViewModel.swift
+++ RxMVVMTodoList/ViewModel/TodoViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import RxDataSources
 import Alamofire
 
 protocol ViewModelType {
@@ -49,56 +50,6 @@ class TodoViewModel: ViewModelType {
         setReloadTrigger() // 장전, 땅!! 하는 중간다리 역할하는놈 setting 하기
     }
     
-    func fetchTodos() { // ViewController에서 호출할 것, 네트워크 콜을 VC에서 VM에게 요청하는 곳이다.
-        apiService.requestGet { todos in print("여기는 fetchTodos: \(todos)") }
-            .subscribe { [weak self] in
-                self?.output.todoList.accept($0)
-            }
-            .disposed(by: disposeBag)
-    }
-    
-    func putTodos(todo: Todo) {
-        var todos = self.output.todoList.value
-        todos.append(todo)
-        apiService.requestPut(params: todos) { todos in
-            // completion
-            print("DB에 저장한 데이터는 다음과 같습니다.")
-            print(todos)
-        }
-        .subscribe(onNext: { print("onNext: \($0)")},
-                     onError: { print("onError = { error_code: \($0._code), description: \($0.localizedDescription)}")},
-                     onCompleted: { print("Save onCompleted")},
-                     onDisposed: { print("Save onDisposed")})
-         .disposed(by: disposeBag)
-
-        fetchTodos()
-//        apiService.requestPut(params: todos, completion: { todos in print("여기는 fetchTodos: \(todos)") })
-//            .subscribe { [weak self] in
-//                self?.input.inputTodoList.
-//            }
-//        apiService.requestPut(params: [], completion: { todos in print("여기는 fetchTodos: \(todos)") })
-//            .subscribe { [weak self] in
-//                self?.input.inputTodoList.accept
-//            }
-    }
-    
-    func deleteTodos(todo: Todo) {
-        var todos = self.output.todoList.value
-        todos = todos.filter{ $0.id != todo.id }
-        apiService.requestPut(params: todos) { todos in
-            // completion
-            print("DB에 저장한 데이터는 다음과 같습니다.")
-            print(todos)
-        }
-        .subscribe(onNext: { print("onNext: \($0)")},
-                     onError: { print("onError = { error_code: \($0._code), description: \($0.localizedDescription)}")},
-                     onCompleted: { print("Save onCompleted")},
-                     onDisposed: { print("Save onDisposed")})
-         .disposed(by: disposeBag)
-
-        fetchTodos()
-    }
-    
     // strong reference cycle의 발생을 막기위해 closure내부에서 self를 사용하면 [weak self]을 항상 명시해주자
     // 이러면 self? 으로 사용하면 된다.
     private func setReloadTrigger() { // ViewModel 생성시 호출된다.
@@ -106,26 +57,54 @@ class TodoViewModel: ViewModelType {
         self.input.reloadTrigger
             .do(onNext: { [weak self] in self?.output.refreshing.onNext(true) })
             .delay(.seconds(3), scheduler: MainScheduler.instance)
-            .flatMapLatest { [weak self] in // requestGet
-                (self?.apiService.requestGet(completion: { todos in print(todos)}))!
+            .flatMapLatest { [weak self] in // requestGet의 이벤트를 기다리게 된다.
+                (self?.apiService.requestGet(completion: { todos in print("여기는 setReloadTrigger: \(todos)")}))!
             }
-            .do(onNext: { [weak self] _ in self?.output.refreshing.onNext(false) })
+            .do(onNext: { [weak self] _ in self?.output.refreshing.onNext(false) }) // refresh
             .bind(to: self.output.todoList) // BehaviorRelay<[Todo]> 에 binding
             .disposed(by: disposeBag)
         // refreshing에 true 이벤트 3초 유지, requestGet, refreshing에 false, 가져온 데이터 todoList에 binding.
     }
     
-//    enum Section: Int, CaseIterable {
-//        case today
-//        case upcoming
-//
-//        var title: String {
-//            switch self {
-//            case .today: return "Today"
-//            default: return "Upcoming"
-//            }
-//        }
-//    }
+    func fetchTodos() { // ViewController에서 호출할 것, 네트워크 콜을 VC에서 VM에게 요청하는 곳이다.
+        apiService.requestGet { todos in print("여기는 fetchTodos: \(todos)") }
+            .subscribe(onNext: { [weak self] in
+                self?.output.todoList.accept($0)
+            }, onError: { error in
+                print("requestGet을 통해 받아온 데이터가 없습니다. error: \(error.localizedDescription)")
+            }, onCompleted: {
+                print("completed. 성공")
+            }, onDisposed: {
+                print("disposed. 리소스 반환")
+            }).disposed(by: disposeBag)
+    }
+    
+    func addTodos(todo: Todo) {
+        var todos = self.output.todoList.value
+        todos.append(todo)
+        self.putTodos(todos: todos)
+    }
+    
+    func deleteTodos(todo: Todo) {
+        var todos = self.output.todoList.value
+        todos = todos.filter{ $0.id != todo.id }
+        self.putTodos(todos: todos)
+    }
+    
+    private func putTodos(todos: [Todo]) {
+        apiService.requestPut(params: todos) { todos in
+            // completion
+            print("DB에 저장한 데이터는 다음과 같습니다.")
+            print(todos)}
+            .subscribe(onNext: { [weak self] in
+                print("onNext: \($0)")
+                self?.output.todoList.accept($0)
+            }, onError: { print("onError = { error_code: \($0._code), description: \($0.localizedDescription)}")
+            }, onCompleted: { print("Save onCompleted")
+            }, onDisposed: { print("Save onDisposed")
+            })
+            .disposed(by: disposeBag)
+    }
 }
 
 
