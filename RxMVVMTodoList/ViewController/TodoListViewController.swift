eb57b97bb41d14430f02b713e70b963ae78bcaef
yeongwoo <jyy0223@naver.com>
2021-09-28 11:32:43 +0900

    add rx datasource

--- RxMVVMTodoList/ViewController/TodoListViewController.swift
+++ RxMVVMTodoList/ViewController/TodoListViewController.swift
@@ -10,22 +10,47 @@ import RxCocoa
 import RxSwift
 import RxDataSources
 
-class TodoListViewController: UIViewController, UICollectionViewDelegateFlowLayout {
-    @IBOutlet weak var collectionView: UICollectionView!
+class TodoListViewController: UIViewController, UIScrollViewDelegate {
+    // RxCocoa 기반 TableView section header을 만들기 위해 편리한 type 지정
+    typealias TodoSectionModel = SectionModel<String, Todo> // defalut로 section과 데이터를 묶기 좋다.
+    typealias TodoDataSource = RxTableViewSectionedReloadDataSource<TodoSectionModel> // RxDataSources에서 제공하는 Sectioned Reload
+    
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var inputViewBottom: NSLayoutConstraint!
     @IBOutlet weak var inputTextField: UITextField!
-
     @IBOutlet weak var isTodayButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
 
-    // [x]TODO: TodoViewModel 만들기
-    let todoListViewModel = TodoViewModel()
-    let disposeBag = DisposeBag()
+    let todoListViewModel = TodoViewModel() // 여기서 ViewModel 생성, View는 ViewModel을 갖고있다.
+    let disposeBag = DisposeBag() // 구독한 뒤 더이상 사용 안하면 리소스 반환하는 쓰레기통
+    
+    private var todoDatasource: TodoDataSource { // RxTableViewSectionedReloadDataSource< SectionModel<String, Todo> >
+        // closure이며 뒤의 3개 파라미터는 datasource에 필요한 cell 구성을 만들기 위함이다.
+        let configureCell: (TableViewSectionedDataSource<TodoSectionModel>, UITableView, IndexPath, Todo) -> UITableViewCell = { (datasource, tableView, indexPath, element) in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TodoListCell.identifier, for: indexPath) as? TodoListCell else { return UITableViewCell() }
+            cell.updateUI(todo: element)
+            cell.deleteButtonTapHandler = { [weak self] in
+                self?.todoListViewModel.deleteTodos(todo: element)
+            }
+            return cell
+        }
+        // cell은 datasource에 사용할 정보를 모두 담고 있고, 이를 datasource에 맞게 initialize를 거친다.
+        // TodoDataSource 객체를 만드는 것이고, 이는 위의 클로저 파라미터로 TableViewSectionedDataSource를 선언해야 하는 이유이다.
+        let datasource = TodoDataSource.init(configureCell: configureCell)
+        // datasource에는 section header을 정의할 수 있고, sectionModels의 index에 해당하는 model을 title로 지정한다.
+        datasource.titleForHeaderInSection = { ds, index in
+            // TodoSectionModel의 모델에 해당하는 String을 이용한다.
+            return ds.sectionModels[index].model
+        }
+        return datasource
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // keyboard Detection
+        // MARK: 이거는 Rx기반 Observable, Observer을 통한 이벤트 전달 방법 존재한지 확인하기
+        // 키보드 등장, 나감을 observe하다가 발생하면 #selector에 해당하는 함수를 실행한다.
         NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillHideNotification, object: nil)
         
