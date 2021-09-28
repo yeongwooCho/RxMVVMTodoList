07fe3ba84cfaf1e86405c27af0a58a6e2b6f32b7
yeongwoo <jyy0223@naver.com>
2021-09-28 11:33:45 +0900

    Fix typo and add comment

--- RxMVVMTodoList/ViewController/TodoListViewController.swift
+++ RxMVVMTodoList/ViewController/TodoListViewController.swift
@@ -54,29 +54,51 @@ class TodoListViewController: UIViewController, UIScrollViewDelegate {
         NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillHideNotification, object: nil)
         
-        setCollectionView() // collection
-        bindCollectionView()
-        todoListViewModel.fetchTodos()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        setLayout() // FlowLayout: cell size
-    }
-    
-    func setCollectionView() {
-        let refreshControl = UIRefreshControl()
-        collectionView.refreshControl = refreshControl
+        // MARK: // delegate 사용을 위한 선언, 솔직하게 왜있는지 모름
+        tableView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
+        // Observable Event는 단방향이기 때문에 Delegate를 통해 값 가져오기가 불가능하다 이것이 rx를 이용한 tableView의 한계이다???
+        // rx를 사용하면 코드가 분산이 되지 않고 한눈에 들어오게 되는 장점이 있는데, delegate는 rx를 쓰더라도 동일한 방식으로 진행된다.
         
-        refreshControl.rx.controlEvent(.valueChanged)
+        
+        setTableView()
+        // table view setting하는 것으로 tableView를 뷰 아래로 당기면 valueChanged가 일어난다.
+        // 이는 Control Event type이며, 이를 구독하고 있다가 이벤트를 받게되면 input.reloadTrigger에 전달한다. 안쓰면 쓰레기통
+        // output.refreshing는 구독 전 데이터도 이벤트로 보낼수 있는 BehaviorSubject이다.
+        // 여기 값이 false이면 refreshing가 완료됨을 TableView에 이벤트로 전달한다.
+        // 사실 이게되려면 input.reloadTrigger가 이벤트를 받는 것과 output.refreshing가 true, false 바뀌는게 연관성이 있어야함.
+        // --> 이게 ViewModel의 private func setReloadTrigger 이다.
+        
+//        bindTableView() // no section
+        // output.todoList는 BehaviorRelay<[Todo]>이고, binding 할 데이터 이기에 이기에 error 이벤트를 전달하지 않는다.
+        // .asDriver(onErrorJustReturn: []) 그래서 여기에 에러가 발생하면 []를 리턴하고, 아니면 [Todo]를 Driver 타입으로 반환한다.
+        // .drive(tableView.rx.items) observable을 구독하는 것이다. 이벤트를 받으면 tableview rx items에 binding을 한다.
+        
+        sectionHeaderBindTableView() // yes section
+        // 일반적인 TableView의 사용은 위와같이 간단하다. 하지만, section Header을 binding할 경우 Delegate patten을 사용해야 한다.
+        // viewModel.output.todoList를 구독하다가 이벤트가 발생하면 viewModel.output.sectionTodoList에 이벤트를 전달한다.
+        // viewModel.output.sectionTodoList 또한 구독중이다. 이벤트가 발생하면 tableView rx items의 datasource에 binding 한다.
+        // todoList가 BehaviorSubject역할을 하고, sectionTodoList가 BehaviorRelay 역할을 한다.
+        // 하지만, Delegate patten 을 빼는경우 section header를 수정할 수도 있어서 일단 이렇게 진행했다.
+        
+        todoListViewModel.fetchTodos() // request Get
+        // requestGet은 Observable이고, 이를 구독하고 있다가 정상적인 Next event를 받게되면 output.todoList에 accept 시키는 동작을 한다.
+    }
+    
+    private func setTableView() {
+        let refreshControl = UIRefreshControl() // 뷰를 아래로 당겨서 새로고침을 제공
+        tableView.refreshControl = refreshControl
+        
+        // Control Event type
+        refreshControl.rx.controlEvent(.valueChanged) // 뷰를 아래로 당기면 reloadTrigger에게 이벤트를 보내 네트워크 콜을 요청한다
             .bind(to: todoListViewModel.input.reloadTrigger) // PublishSubject<Void>
             .disposed(by: disposeBag)
         
         self.todoListViewModel.output.refreshing // BehaviorSubject<Bool>
             .subscribe(onNext: { [weak self] refreshing in
-                if refreshing { return } // refreshing is true
-                self?.collectionView.refreshControl?.endRefreshing()
-            })
+                if refreshing { return } // refreshing이 true이면 계속 구독
+                self?.tableView.refreshControl?.endRefreshing() // 새로고침 종료를 의미
+            }) // output.refreshing 값이 바꼈다는 것은 정상적으로 데이터를 Get했다는 것
             .disposed(by: disposeBag)
     }
     
@@ -89,29 +111,9 @@ class TodoListViewController: UIViewController, UIScrollViewDelegate {
                 // MARK: delete button Tap Handler
                 cell.deleteButtonTapHandler = { [weak self] in
                     self?.todoListViewModel.deleteTodos(todo: item)
-                    self?.todoListViewModel.fetchTodos() 
                 }
                 return cell
             }
-            .disposed(by: disposeBag)
-        
-        collectionView.rx.itemSelected
-            .subscribe(onNext: { [weak self] indexPath in
-                print(1)
-                guard let detailVC = UIComponents.mainStoryboard.instantiateViewController(withIdentifier: TodoDetailViewController.identifier) as? TodoDetailViewController else { return }
-                let selectedTodo = self?.todoListViewModel.output.todoList.value[indexPath.item]
-                detailVC.todoDetailViewModel.input.todoInfo.onNext(selectedTodo)
-                self?.present(detailVC, animated: true, completion: nil)
-            })
-            .disposed(by: disposeBag)
-    }
-    
-    func setLayout() {
-        // UICollectionViewDelegateFlowLayout
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.itemSize = CGSize(width: view.bounds.width, height: 70)
-        collectionView.setCollectionViewLayout(flowLayout, animated: true)
-        collectionView.rx.setDelegate(self)
             .disposed(by: disposeBag)
     }
 
@@ -119,16 +121,11 @@ class TodoListViewController: UIViewController, UIScrollViewDelegate {
         // 투데이 버튼 토글 작업
         isTodayButton.isSelected = !isTodayButton.isSelected
     }
-
-    // MARK: 여기 수정해야함
+    
     @IBAction func addTaskButtonTapped(_ sender: Any) {
         guard let detail = inputTextField.text, detail.isEmpty == false else { return }
-//        let todo = TodoManager.shared.createTodo(detail: detail, isToday: isTodayButton.isSelected)
-//        todoListViewModel.addTodo(todo)
-//        collectionView.reloadData()
         let todo = Todo(detail: detail, isToday: isTodayButton.isSelected)
-        todoListViewModel.putTodos(todo: todo)
-        todoListViewModel.fetchTodos()
+        todoListViewModel.addTodos(todo: todo)
         inputTextField.text = ""
         isTodayButton.isSelected = false
     }
@@ -139,6 +136,8 @@ class TodoListViewController: UIViewController, UIScrollViewDelegate {
     }
 }
 
+// rx 기반이 아닌 NotificationCenter에 의한 이벤트 전달방식
+// MARK: rx 기반 있는지 확인하고, 적용하기
 extension TodoListViewController {
     @objc private func adjustInputView(noti: Notification) {
         guard let userInfo = noti.userInfo else { return }
