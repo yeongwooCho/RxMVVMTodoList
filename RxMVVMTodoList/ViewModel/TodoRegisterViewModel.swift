d3b1476eb06279c8c0a6cf16e7058200ff7036d9
yeongwoo <jyy0223@naver.com>
2021-09-30 09:49:29 +0900

    add todo register view model

---
+++ RxMVVMTodoList/ViewModel/TodoRegisterViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TodoRegisterViewModel.swift
+//  RxMVVMTodoList
+//
+//  Created by 조영우 on 2021/09/29.
+//
+
+import Foundation
+
+// register에서 필요한 비지니스 로직
+// 등록에 필요한 기능이 들어간다.
+// 결국 날짜 선택에 대한 이벤트를 던져야 한다.
