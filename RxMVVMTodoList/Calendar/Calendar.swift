d929f6ac8c9e6eb0817d86391eb39983ce7649a7
yeongwoo <jyy0223@naver.com>
2021-10-01 15:53:57 +0900

    fix calendar appearance color

--- RxMVVMTodoList/Calendar/Calendar.swift
+++ RxMVVMTodoList/Calendar/Calendar.swift
@@ -17,31 +17,15 @@ class Calendar {
     }
     
     func setLayoutCalendar(frameView: UIView) -> FSCalendar {
-        
-//        let frame: CGRect = CGRect(x: frameView.bounds.minX, y: frameView.bounds.minY, width: frameView.bounds.width, height: frameView.bounds.height)
-//        let calendar = FSCalendar(frame: frame)
         let calendar = FSCalendar(frame: frameView.frame)
-        // 달력의 평일 날짜 색깔
-        calendar.appearance.titleDefaultColor = .black // 달력의 토,일 날짜 색깔
-        calendar.appearance.titleWeekendColor = .red // 달력의 맨 위의 년도, 월의 색깔
-        calendar.appearance.headerTitleColor = .systemPink // 달력의 요일 글자 색깔
-        calendar.appearance.weekdayTextColor = .orange
         
-        // 달력의 년월 글자 바꾸기
-        calendar.appearance.headerDateFormat = "YYYY년 M월"
-
-        // 달력의 요일 글자 바꾸는 방법 1
-        calendar.locale = Locale(identifier: "ko_KR")
-                
-        // 달력의 요일 글자 바꾸는 방법 2
-        calendar.calendarWeekdayView.weekdayLabels[0].text = "일"
-        calendar.calendarWeekdayView.weekdayLabels[1].text = "월"
-        calendar.calendarWeekdayView.weekdayLabels[2].text = "화"
-        calendar.calendarWeekdayView.weekdayLabels[3].text = "수"
-        calendar.calendarWeekdayView.weekdayLabels[4].text = "목"
-        calendar.calendarWeekdayView.weekdayLabels[5].text = "금"
-        calendar.calendarWeekdayView.weekdayLabels[6].text = "토"
-        
+        calendar.appearance.titleDefaultColor = .black // 달력의 평일 날짜 색깔
+        calendar.appearance.titleWeekendColor = .red // 달력의 토,일 날짜 색깔
+        calendar.appearance.headerTitleColor = #colorLiteral(red: 0.1812617481, green: 0.3442890644, blue: 0.9237771034, alpha: 1) // 달력의 맨 위의 년도, 월의 색깔
+        calendar.appearance.weekdayTextColor = #colorLiteral(red: 0.1812617481, green: 0.3442890644, blue: 0.9237771034, alpha: 1) // 달력의 요일 글자 색깔
+        calendar.appearance.selectionColor = #colorLiteral(red: 0.1812617481, green: 0.3442890644, blue: 0.9237771034, alpha: 1) // 선택했을때 색깔
+        calendar.appearance.headerDateFormat = "YYYY년 M월" // 달력의 년월 글자 바꾸기
+        calendar.locale = Locale(identifier: "ko_KR") // 한국달력
         return calendar
     }
     
