b571266c3b6442065e79985d114cfb41dd2a980e
yeongwoo <jyy0223@naver.com>
2021-10-01 15:56:30 +0900

    fix todo property from isToday to start, deadline date

--- RxMVVMTodoList/Model/Todo.swift
+++ RxMVVMTodoList/Model/Todo.swift
@@ -12,32 +12,29 @@ struct Todo: Codable, Equatable {
         case detail
         case id
         case isDone
-        case isToday
+        case startDate
+        case deadlineDate
     }
     
     var detail: String // 내용
     var id: TimeInterval // 구분 by timeIntervalSince1970
     var isDone: Bool // select button
-    var isToday: Bool // today, upcoming
+    var startDate: String
+    var deadlineDate: String
     
-    init(detail: String, id: TimeInterval, isDone: Bool, isToday: Bool) {
-        self.detail = detail
-        self.id = id
-        self.isDone = isDone
-        self.isToday = isToday
-    }
-    
-    init (detail: String, isToday: Bool) {
+    init(detail: String, isDone: Bool, startDate: String, deadlineDate: String) {
         self.detail = detail
         self.id = Date().timeIntervalSince1970
-        self.isDone = false
-        self.isToday = isToday
+        self.isDone = isDone
+        self.startDate = startDate
+        self.deadlineDate = deadlineDate
     }
     
-    mutating func update(detail: String, isDone: Bool, isToday: Bool) {
+    mutating func update(detail: String, isDone: Bool, startDate: String, deadlineDate: String) {
         self.detail = detail
         self.isDone = isDone
-        self.isToday = isToday
+        self.startDate = startDate
+        self.deadlineDate = deadlineDate
     }
     
     // 동등 조건 추가
