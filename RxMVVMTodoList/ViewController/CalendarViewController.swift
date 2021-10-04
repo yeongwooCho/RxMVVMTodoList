88fe94e378a65ca3abf5934a6682bdd63e344379
yeongwoo <jyy0223@naver.com>
2021-10-04 23:19:54 +0900

    fix from one page to two page in calendar view controller

--- RxMVVMTodoList/ViewController/CalendarViewController.swift
+++ RxMVVMTodoList/ViewController/CalendarViewController.swift
@@ -7,41 +7,63 @@
 
 import UIKit
 import FSCalendar
+import RxSwift
 
 class CalendarViewController: UIViewController {
-
+    static let identifier = "CalendarViewController"
+    
     @IBOutlet weak var calendar: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var selectButton: UIButton!
+    
+    var titleText: String?
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupButtonTap()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateUI()
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         setupCalendar()
     }
-
+    
+    func updateUI() {
+        guard let titleString = titleText else { return }
+        titleLabel.text = titleString
+    }
+    
+    func setupButtonTap() {
+        selectButton.rx.tap
+            .subscribe(onNext: { [weak self] buttonEvent in
+                self?.presentingViewController?.dismiss(animated: true, completion: nil)
+            })
+            .disposed(by: disposeBag)
+    }
 }
 
 extension CalendarViewController: FSCalendarDataSource, FSCalendarDelegate {
     private func setupCalendar() {
-//        calendarView.
         let calendar = Calendar(frameView: calendar).getCalendar()
-//        calendarView.dataSource = self
-//        calendarView.delegate = self
-//        view.addSubview(Calendar(frameView: calendarView).getCalendar())
-//        let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: 320, height: 300))
         calendar.dataSource = self
         calendar.delegate = self
         view.addSubview(calendar)
-//        self.calendar = calendar
-
+        self.calendar = calendar
     }
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        // 날짜를 원하는 형식으로 저장하기 위한 방법입니다.
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        let selectDate = dateFormatter.string(from: date)
-        print(selectDate)
+        if self.titleText == "Start Date" {
+            RegisterTodoViewModel.shared.output.startDateRelay
+                .accept(Date.dateString(date: date))
+        } else {
+            RegisterTodoViewModel.shared.output.deadlineDateRelay
+                .accept(Date.dateString(date: date))
+        }
     }
 }
