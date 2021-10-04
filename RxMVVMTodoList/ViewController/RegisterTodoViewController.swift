fbd3e84b147297f2e5eae363ee53ca6c3a4bb1b8
yeongwoo <jyy0223@naver.com>
2021-10-04 23:15:21 +0900

    fix button tap logic in register view controller

--- RxMVVMTodoList/ViewController/RegisterTodoViewController.swift
+++ RxMVVMTodoList/ViewController/RegisterTodoViewController.swift
@@ -6,35 +6,69 @@
 //
 
 import UIKit
-import FSCalendar
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 class RegisterTodoViewController: UIViewController {
-
-    fileprivate weak var calendar: FSCalendar!
+    static let identifier = "RegisterTodoViewController"
     
-//    @IBOutlet weak var calendarView: FSCalendar!
     @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var headerLabel: UILabel!
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var startDateLabel: UILabel!
     @IBOutlet weak var startDateAddButton: UIButton!
-    
     @IBOutlet weak var deadLineDateLabel: UILabel!
     @IBOutlet weak var deadLineDateAddButton: UIButton!
     
+    let registerTodoViewModel = RegisterTodoViewModel.shared
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
-    
-    override func viewDidLayoutSubviews() {
-        
+        setupDateLabel()
+        setupAddButtonSelected()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        setupLayout()
+    }
+    
+    private func setupDateLabel() {
+        registerTodoViewModel.output.startDateRelay
+            .asDriver(onErrorJustReturn: "")
+            .drive(startDateLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        registerTodoViewModel.output.deadlineDateRelay
+            .asDriver(onErrorJustReturn: "")
+            .drive(deadLineDateLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
+    private func setupAddButtonSelected() {
+        startDateAddButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let calendarVC = UIComponents.mainStoryboard.instantiateViewController(withIdentifier: CalendarViewController.identifier) as? CalendarViewController else { return }
+                calendarVC.titleText = "Start Date"
+                calendarVC.modalPresentationStyle = .fullScreen
+                self?.present(calendarVC, animated: true, completion: nil)
+            })
+            .disposed(by: disposeBag)
+            
+        deadLineDateAddButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let calendarVC = UIComponents.mainStoryboard.instantiateViewController(withIdentifier: CalendarViewController.identifier) as? CalendarViewController else { return }
+                calendarVC.titleText = "Deadline Date"
+                calendarVC.modalPresentationStyle = .fullScreen
+                self?.present(calendarVC, animated: true, completion: nil)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func setupLayout() {
         titleTextField.layer.borderWidth = 1
         titleTextField.layer.cornerRadius = 10
         startDateLabel.layer.borderWidth = 1
@@ -44,24 +78,4 @@ class RegisterTodoViewController: UIViewController {
         deadLineDateLabel.layer.cornerRadius = 10
         deadLineDateAddButton.layer.cornerRadius = 10
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-    
-    private func setupItemSelected() {
-//        startDateAddButton.rx.tap
-//            .single()
-        
-//        tableView.rx.itemSelected
-//            .subscribe(onNext: { [weak self] indexPath in
-//                guard let detailVC = UIComponents.mainStoryboard.instantiateViewController(withIdentifier: TodoDetailViewController.identifier) as? TodoDetailViewController else { return }
-//                let selectedTodo = self?.todoListViewModel.output.todoList.value[indexPath.item]
-//                detailVC.todoDetailViewModel.input.todoInfo.onNext(selectedTodo)
-//                detailVC.modalPresentationStyle = .fullScreen
-//                self?.present(detailVC, animated: true, completion: nil)
-//            })
-//            .disposed(by: disposeBag)
-    }
 }
-
