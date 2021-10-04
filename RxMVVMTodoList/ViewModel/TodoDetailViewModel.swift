43c975c7b4b965c430ab19d8d72daa87045d0703
yeongwoo <jyy0223@naver.com>
2021-10-04 23:16:22 +0900

    fix detail view model to comment

--- RxMVVMTodoList/ViewModel/TodoDetailViewModel.swift
+++ RxMVVMTodoList/ViewModel/TodoDetailViewModel.swift
@@ -8,46 +8,46 @@
 import UIKit
 import RxCocoa
 import RxSwift
-
-class TodoDetailViewModel { // : ViewModelType
-    
-    struct Input {
-        let todoInfo = BehaviorSubject<Todo?>(value: nil)
-    }
-    
-    struct Output {
-//        var todo: Observable<Todo>
-        var detail: Observable<String>
-        var id: Observable<TimeInterval>
-        var isDone: Observable<Bool>
-        var isToday: Observable<Bool>
-    }
-    
-    var input: Input
-    var output: Output
-    
-    
-    init() {
-        self.input = Input()
-        
-//        let todo = self.input.todoInfo
-        
-        let detail = self.input.todoInfo
-            .filter { $0 != nil }
-            .map { $0!.detail }
-        
-        let id = self.input.todoInfo
-            .filter { $0 != nil }
-            .map { $0!.id }
-        
-        let isDone = self.input.todoInfo
-            .filter { $0 != nil }
-            .map { $0!.isDone }
-        
-        let isToday = self.input.todoInfo
-            .filter { $0 != nil }
-            .map { $0!.isToday }
-        
-        self.output = Output(detail: detail, id: id, isDone: isDone, isToday: isToday)
-    }
-}
+//
+//class TodoDetailViewModel { // : ViewModelType
+//    
+//    struct Input {
+//        let todoInfo = BehaviorSubject<Todo?>(value: nil)
+//    }
+//    
+//    struct Output {
+////        var todo: Observable<Todo>
+//        var detail: Observable<String>
+//        var id: Observable<TimeInterval>
+//        var isDone: Observable<Bool>
+//        var isToday: Observable<Bool>
+//    }
+//    
+//    var input: Input
+//    var output: Output
+//    
+//    
+//    init() {
+//        self.input = Input()
+//        
+////        let todo = self.input.todoInfo
+//        
+//        let detail = self.input.todoInfo
+//            .filter { $0 != nil }
+//            .map { $0!.detail }
+//        
+//        let id = self.input.todoInfo
+//            .filter { $0 != nil }
+//            .map { $0!.id }
+//        
+//        let isDone = self.input.todoInfo
+//            .filter { $0 != nil }
+//            .map { $0!.isDone }
+//        
+//        let isToday = self.input.todoInfo
+//            .filter { $0 != nil }
+//            .map { $0!.isToday }
+//        
+//        self.output = Output(detail: detail, id: id, isDone: isDone, isToday: isToday)
+//    }
+//}
