02fca92a0529ac6b807c6e7b09e7ad0de774394b
yeongwoo <jyy0223@naver.com>
2021-10-05 11:04:06 +0900

    add set bind logic in todo detail view controller

--- RxMVVMTodoList/ViewController/TodoDetailViewController.swift
+++ RxMVVMTodoList/ViewController/TodoDetailViewController.swift
@@ -12,58 +12,30 @@ class TodoDetailViewController: UIViewController {
 
     static let identifier = "TodoDetailViewController"
 
-//    @IBOutlet weak var detailLabel: UILabel!
-//    @IBOutlet weak var idLabel: UILabel!
-//    @IBOutlet weak var isDoneLabel: UILabel!
-//    @IBOutlet weak var isTodayLabel: UILabel!
-    let detailLabel = UILabel()
-    let idLabel = UILabel()
-    let isDoneLabel = UILabel()
-    let isTodayLabel = UILabel()
-    let closeButton = UIButton()
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var registerTimeLabel: UILabel!
+    @IBOutlet weak var startDateLabel: UILabel!
+    @IBOutlet weak var deadlineDateLabel: UILabel!
     
-    
-//    let todoDetailViewModel = TodoDetailViewModel()
+    let todoDetailViewModel = TodoDetailViewModel()
     
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        bindUI()
+        setBindUI()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-//        closeButton
-    }
-    @IBAction func close(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    func bindUI() {
-//        todoDetailViewModel.output.detail
-//            .subscribe(onNext: { [weak self] detail in
-//                self?.detailLabel.text = detail
-//            })
-//            .disposed(by: disposeBag)
-//        
-//        todoDetailViewModel.output.id
-//            .subscribe(onNext: { [weak self] id in
-//                self?.idLabel.text = String(id)
-//            })
-//            .disposed(by: disposeBag)
-//        
-//        todoDetailViewModel.output.isDone
-//            .subscribe(onNext: { [weak self] isDone in
-//                self?.isDoneLabel.text = String(isDone)
-//            })
-//            .disposed(by: disposeBag)
-//        
-//        todoDetailViewModel.output.isToday
-//            .subscribe(onNext: { [weak self] isToday in
-//                self?.isTodayLabel.text = String(isToday)
-//            })
-//            .disposed(by: disposeBag)
+    func setBindUI() {
+        todoDetailViewModel.output.todo
+            .subscribe(onNext: { [weak self] todo in
+                self?.detailLabel.text = todo.detail
+                self?.idLabel.text = String(todo.id)
+                self?.registerTimeLabel.text = Date.dateToString(date: Date(timeIntervalSince1970: todo.id), isTime: true)
+                self?.startDateLabel.text = todo.startDate
+                self?.deadlineDateLabel.text = todo.deadlineDate
+            })
+            .disposed(by: disposeBag)
     }
 }
