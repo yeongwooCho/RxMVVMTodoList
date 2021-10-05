7676d4f03e7bc0a7a78028f22736f4e3211f291c
yeongwoo <jyy0223@naver.com>
2021-10-05 10:52:39 +0900

    add condition in dateToString

--- RxMVVMTodoList/Extention/Date.swift
+++ RxMVVMTodoList/Extention/Date.swift
@@ -9,10 +9,15 @@ import Foundation
 import UIKit
 
 extension Date {
-    static func dateString(date: Date) -> String {
+    static func dateToString(date: Date, isTime: Bool) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ko_kr")
-        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+        if isTime {
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        } else {
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+        }
         return dateFormatter.string(from: date)
     }
 }
