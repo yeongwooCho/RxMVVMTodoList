27da6239a6f3e58890b2bd648c216163fbfd284f
yeongwoo <jyy0223@naver.com>
2021-10-05 11:01:08 +0900

    add request put in register view model

--- RxMVVMTodoList/ViewModel/RegisterTodoViewModel.swift
+++ RxMVVMTodoList/ViewModel/RegisterTodoViewModel.swift
@@ -15,6 +15,7 @@ protocol RegisterViewModelType {
     
     var input: Input { get }
     var output: Output { get }
+    var apiService: RxNetworkCallInterface { get } // protocol
     var disposeBag: DisposeBag { get }
 }
 
@@ -23,76 +24,82 @@ class RegisterTodoViewModel: RegisterViewModelType {
     static let shared = RegisterTodoViewModel()
     
     struct Input {
-        let reloadDate = PublishSubject<String>()
+        let registerTodo = PublishSubject<Todo>() // register todo 할때 이용
     }
     
     struct Output {
-        var startDateRelay = BehaviorRelay<String>(value: Date.dateString(date: Date()))
-        var deadlineDateRelay = BehaviorRelay<String>(value: Date.dateString(date: Date(timeIntervalSinceNow: 86400)))
+        var startDateRelay = BehaviorRelay<String>(value: Date.dateToString(date: Date(), isTime: false))
+        var deadlineDateRelay = BehaviorRelay<String>(value: Date.dateToString(date: Date(timeIntervalSinceNow: 86400), isTime: false))
     }
     
     var input: Input
     var output: Output
+    var apiService: RxNetworkCallInterface // protocol
     var disposeBag: DisposeBag
     
-    init(input: Input = Input(), output: Output = Output()) {
+    init(input: Input = Input(), output: Output = Output(), apiService: RxNetworkCallInterface = AlamofireRxCall()) {
         print("registerViewModel 리소스 생성되었습니다.")
         self.input = input
         self.output = output
+        self.apiService = apiService
         self.disposeBag = DisposeBag()
-        setDateRelay()
+        setRegisterTodo()
     }
     
     deinit {
         print("registerViewModel 리소스 반환되었습니다.")
     }
     
-    func setDateRelay() {
-//        self.input.reloadDate
-//            .do(onNext: { str in print("여기 나오면 승산있다. \(str)") })
-//            .bind(to: self.output.startDateRelay)
-//            .disposed(by: disposeBag)
-        
-//        self.input.reloadDate
-//            .subscribe(onNext: { [weak self] data in
-//                if data.contains("Start Date|") {
-//                    data.split(separator: "|")[1]
-//                    self?.output.startDateRelay
-//                        .accept(<#T##event: String##String#>)
-//                } else {
-//                    se
-//                }
-//                
-//            })
-//            .disposed(by: disposeBag)
-//        calendarViewModel.output.startDateRelay
-//            .subscribe(onNext: { [weak self] date in
-//                self?.output.startDateRelay.accept(date)
-//                print("여기는 registerViewModel에서 calendarViewModel.output.startDateRelay 상태를 파악하는 곳이다. \(String(describing: self?.calendarViewModel.output.startDateRelay.value))")
-//            })
-//            .disposed(by: disposeBag)
-        
-        
-//        calendarViewModel.output.startDateRelay
-//            .bind(to: self.output.startDateRelay)
-//            .disposed(by: disposeBag)
-        
-//        calendarViewModel.output.startDateRelay
-//        self.output.startDateRelay
-//            .subscribe(onNext: { date in
-//                print("registerViewModel에서 date를 출력하면 다음과 같다.")
-//                print(date)
-////                print(self?.calendarViewModel.output.startDateRelay.value ?? "default value")
-//            })
-//            .disposed(by: disposeBag)
-//        
-////        calendarViewModel.output.deadlineDateRelay
-//        self.output.deadlineDateRelay
-//            .subscribe(onNext: { date in
-////                self?.output.deadlineDateRelay.accept(date)
-////                print("여기는 registerViewModel에서 calendarViewModel.output.deadlineDateRelay 상태를 파악하는 곳이다. \(String(describing: self?.calendarViewModel.output.deadlineDateRelay.value))")
-//                
-//            })
-//            .disposed(by: disposeBag)
+    private func setRegisterTodo() {
+        self.input.registerTodo
+            .subscribe(onNext: { [weak self] todo in
+                self?.addTodos(todo: todo)
+                print("Add task button에 의한 데이터 받음 \(todo)")
+            }, onError: { error in
+                print("error: \(error.localizedDescription)")
+            }, onCompleted: {
+                print("register todo completed")
+            }, onDisposed: {
+                print("register todo disposed")
+            })
+            .disposed(by: disposeBag)
+    }
+            
+    private func addTodos(todo: Todo) {
+        apiService.requestGet { todos in
+            print("register requestGet: Todos == \(todos)")
+        }
+        .subscribe(onNext: { [weak self] todos in
+            if todos == [] {
+                self?.putTodos(todos: [todo])
+            } else {
+                var registerTodos = todos
+                registerTodos.append(todo)
+                self?.putTodos(todos: registerTodos)
+            }
+        }, onError: { error in
+            print("add todos error: \(error.localizedDescription)")
+        }, onCompleted: {
+            print("register add todos completed.")
+        }, onDisposed: {
+            print("register add todos disposed.")
+        })
+        .disposed(by: disposeBag)
+    }
+    
+    private func putTodos(todos: [Todo]) {
+        apiService.requestPut(params: todos) { todos in
+            // completion
+            print("DB에 저장한 데이터는 다음과 같습니다.")
+            print(todos)}
+            .subscribe(onNext: { todos in
+                print("onNext: \(todos)")
+//                self?.output.todoList.accept($0)
+                // MARK: 이거 해결하면 실시간 데이터 띄우기 가능
+            }, onError: { print("onError = { error_code: \($0._code), description: \($0.localizedDescription)}")
+            }, onCompleted: { print("Save onCompleted")
+            }, onDisposed: { print("Save onDisposed")
+            })
+            .disposed(by: disposeBag)
     }
 }
