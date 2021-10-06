16bf31f3eed487ae4d1ea7cdb1611dc819fdbb85
yeongwoo <jyy0223@naver.com>
2021-10-06 11:15:40 +0900

    fix generic type in network call

--- RxMVVMTodoList/NetworkService/Alamofire+Rx.swift
+++ RxMVVMTodoList/NetworkService/Alamofire+Rx.swift
@@ -11,28 +11,25 @@ import RxCocoa
 import Alamofire
 
 protocol RxNetworkCallInterface {
+    typealias T = Todo
     var url: String { get }
-    func requestGet(completion: @escaping ([Todo]) -> Void) -> Single<[Todo]>
-    func requestPut(params: [Todo], completion: @escaping ([Todo]) -> Void) -> Single<[Todo]>
+    func requestGet(completion: @escaping ([T]) -> Void) -> Single<[T]>
+    func requestPut(params: [T], completion: @escaping ([T]) -> Void) -> Single<[T]>
 }
 
 class AlamofireRxCall: RxNetworkCallInterface {
-    
-    enum GetFailureReason: Int, Error {
-        case unAuthorized = 401
-        case notFound = 404
-    }
-    
-    var url = "https://sendy-todolist-default-rtdb.asia-southeast1.firebasedatabase.app/todos.json"
+    var url = "https://rxtodolist-6d4e2-default-rtdb.asia-southeast1.firebasedatabase.app/todos.json"
+//    var url = "https://sendy-todolist-default-rtdb.asia-southeast1.firebasedatabase.app/todos.json"
 
-    func requestGet(completion: @escaping ([Todo]) -> Void) -> Single<[Todo]> {
-        return Single<[Todo]>.create { (single) -> Disposable in
+    func requestGet(completion: @escaping ([T]) -> Void) -> Single<[T]> {
+        return Single<[T]>.create { (single) -> Disposable in
             AF.request(self.url, // alamofire를 활용한 request
                        method: .get)
                 .validate(statusCode: 200..<300) // 200 ~ 299
                 .responseJSON(completionHandler: { response in // 응답은 json으로 받는다
                     switch response.result {
                     case .success(let obj):
+                        
                         guard let objArray = obj as? [[String: Any]]  else { // parsing부분 json -> data
                             print("Parsing되는 데이터가 DB에 존재하지 않는다.")
                             single(.success([]))
@@ -43,6 +40,7 @@ class AlamofireRxCall: RxNetworkCallInterface {
                             let data = try JSONSerialization.data(withJSONObject: objArray, options: [])
                             let decoder = JSONDecoder()
                             let todos = try decoder.decode([Todo].self, from: data)
+                            print("success requestGet: \(todos)")
                             single(.success(todos))
                             completion(todos) // escaping closure. 함수 실행이 완료되었을때 실행된다.
                         } catch let error {
@@ -58,8 +56,8 @@ class AlamofireRxCall: RxNetworkCallInterface {
         }
     }
     
-    func requestPut(params: [Todo], completion: @escaping ([Todo]) -> Void) -> Single<[Todo]> {
-        return Single<[Todo]>.create { single in
+    func requestPut(params: [T], completion: @escaping ([T]) -> Void) -> Single<[T]> {
+        return Single<[T]>.create { single in
             AF.request(self.url,
                        method: .put,
                        parameters: params,
@@ -68,7 +66,7 @@ class AlamofireRxCall: RxNetworkCallInterface {
                 .validate(statusCode: 200..<300)
                 .responseString { response in
                     switch response.result {
-                    case .success:
+                    case .success: // 성공했다면 put한 데이터를 이벤트로 보낸다.
                         single(.success(params))
                         completion(params)
                     case .failure(let error):
